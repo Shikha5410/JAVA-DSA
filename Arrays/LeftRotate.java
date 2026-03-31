adca625f1fddc415d5c70329680687267d28dbcd
Shikha5410 <shikhakushwaha712@gmail.com>
2026-03-31 20:15:32 +0530

    Added array programs

---
+++ Arrays/LeftRotate.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class LeftRotate{
+    public static void reverse(int arr[], int first, int end){
+        while(first<end){
+            int temp = arr[first];
+            arr[first] = arr[end];
+            arr[end] = temp;
+
+            first++;
+            end--;
+        }
+    }
+    public static void left_rotate(int arr[],int k){
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        reverse(arr,0,n-1);
+    }
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+        int key = 2;
+
+        left_rotate(arr,key);
+        for(int num : arr){
+            System.out.print(num +" ");
+        }
+    }
+}
