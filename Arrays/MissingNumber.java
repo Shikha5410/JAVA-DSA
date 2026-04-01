f41caef8c51b61ba3e837859be8cc440d51fa501
Shikha5410 <shikhakushwaha712@gmail.com>
2026-04-01 21:21:41 +0530

    Update MissingNumber.java
    Improved code readability with comments

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -1,12 +1,14 @@
 public class MissingNumber{
+    // find missing number using sum method
     public static int missing_number(int arr[]){
-        int n = arr.length +1;
-        int total = n*(n+1)/2;
+        int n = arr.length +1; //one number is missing
+        
+        int total = n*(n+1)/2; //sum of 1 to n
         int sum = 0;
         for(int num :arr){
            sum+=num;
         }
-        return total-sum;
+        return total-sum; //missing number
     }
     public static void main (String args[]){
         int arr[] = {1,2,4,5};
