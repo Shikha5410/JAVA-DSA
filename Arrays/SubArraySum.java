80a7f623464aa109bc9bff192f3e4ffc04506764
shikhaKushwahaDev <shikhakushwaha712@gmail.com>
2026-05-02 20:24:21 +0530

    Update SubArraySum.java

--- Arrays/SubArraySum.java
+++ Arrays/SubArraySum.java
@@ -1,21 +1,26 @@
 public class SubArraySum{
+    // Function to calculate the sum of all Subarrays
     public static void subarraysum(int arr[]){
-        int n = arr.length;
-        int total = 0;
-        
-        for(int i=0;i<n;i++){    //start index
-            
-            for(int j=i;j<n;j++){   //end index
-                int sum = 0;
+        int n = arr.length; // size of array
+        int total = 0; // stores total sum of all subarrays
 
-                for(int k=i; k<=j ; k++){    //print subarray
-                    System.out.print(arr[k] +" ");
-                    sum += arr[k];
+        // Outer loop -> starting index of subarray
+        for(int i=0;i<n;i++){ 
+            
+         // Middle loop -> ending index of subarray
+            for(int j=i;j<n;j++){   
+                int sum = 0; // sum of current subarray
+
+                // Inner loop -> prints elements from i to j  
+                  for(int k=i;k<=j;k++){
+                    System.out.print(arr[k] +" "); // print element
+                    sum += arr[k]; // add element to current sum
                 }
-                total += sum;
-                System.out.println();
+                total += sum; // add current subarray sum to total
+                System.out.println(); // move to next line
             }
         }
+        // Print final total sum of all subarrays
         System.out.print("Total sum of subarray :" + total);
     }
     public static void main (String args[]){
